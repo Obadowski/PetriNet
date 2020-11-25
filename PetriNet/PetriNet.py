c6116b48524e161e45d2aa0d573c986bfe5559b3
Obadowski <8399Chato>
2020-11-25 15:01:38 -0300

    A mock unit test file is included. Prototypes for all classes necessary to build a Petri are available in the PetriNet.py.

---
+++ PetriNet/PetriNet.py
@@ -0,0 +1,38 @@
+# Implementantion of all PetriNet class
+# 1st class to be implemented is Petri Net Basic (or classic Petri Net)
+
+class PetriNet(object):
+    """
+    This class contains all major object necessary to describe a complete Petri Net
+    """
+    pass
+
+
+class Place(object):
+    """
+    Class Place implements the behavior of the similar structure within a Petri Net
+
+    """
+    pass
+
+
+class Transition(object):
+    """
+    Class Transition implements the behavior of the same name structure within a Petri Net
+    """
+    pass
+
+
+class Arc(object):
+    """
+    Class Arc implements the behavior of the same name structure within a Petri Net
+    """
+    pass
+
+
+class InhibitorArc(Arc):
+    """
+    InhibitorArc implements the inhibitor arc behavior.
+    It extends the features presents in the Arc class
+    """
+    pass
